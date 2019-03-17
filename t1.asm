d887817566b166ed55db162251ae5af2f937b75b
JVSRamos <joaoramosj@usp.br>
2019-03-17 17:17:20 -0300

    added the fibo to the main

--- t1.asm
+++ t1.asm
@@ -118,7 +118,7 @@ main:	# in�cio do programa
 	
 	li $t1, 7
 	beq $t0, $t1, imc
-	
+
 	li $t1, 8
 	beq $t0, $t1, fat
 	
@@ -173,42 +173,42 @@ tab:
 	addi $t3, $zero 11 #stop condtion
 	
 			
-loop_tab :
+	loop_tab :
 	
-	beq $t1,$t3,end_loop_tab
+		beq $t1,$t3,end_loop_tab
 					
-	li $v0,1 
-	move $a0, $a1 #print the parameter number
-	syscall
+		li $v0,1 
+		move $a0, $a1 #print the parameter number
+		syscall
 	
-	li $v0, 4
-	la $a0, times # print x
-	syscall
+		li $v0, 4
+		la $a0, times # print x
+		syscall
 	
-	li $v0,1 
-	move $a0, $t1 #print the actual number
-	syscall
+		li $v0,1 
+		move $a0, $t1 #print the actual number
+		syscall
 	
-	li $v0, 4
-	la $a0, equals #print =
-	syscall
+		li $v0, 4
+		la $a0, equals #print =
+		syscall
 	
-	li $v0,1
-	mul $t2, $a1, $t1 #print a * i
-	move $a0, $t2 #print the result
-	syscall
+		li $v0,1
+		mul $t2, $a1, $t1 #print a * i
+		move $a0, $t2 #print the result
+		syscall
 	
-	li $v0, 4
-	la $a0, n #print \n
-	syscall
+		li $v0, 4
+		la $a0, n #print \n
+		syscall
 	
-	addi $t1,$t1, 1 #go to the next number
+		addi $t1,$t1, 1 #go to the next number
 	
-	j loop_tab 
+		j loop_tab 
 	
-end_loop_tab :
+	end_loop_tab :
 	
-	j main
+		j main
 	
 imc:
 
@@ -218,9 +218,86 @@ fat:
 
 	j main
 	
-fib:
-
+fib :
+		
+	li $v0,4
+	la $a0,str_n1 #print n1 =
+	syscall
+		
+	li $v0,5
+	syscall #read the first number
+	move $a1,$v0
+		
+	li $v0,4
+	la $a0,str_n2 #print n2 =
+	syscall
+		
+	li $v0,5
+	syscall #read the second number
+	move $a2,$v0
+	
+	jal fib_seg
 	j main
+	
+fib_seg:
+
+	addi $sp,$sp,-12 
+	sw $a1, 0($sp) #the first argument
+	sw $a2, 4($sp) #the second argument		
+	sw $ra, 8($sp)
+	
+	move $t4,$a2 #stop condition
+	addi $t4,$t4, 1 #end the loop at a-1
+	
+	loop_seg : 
+		beq $a1,$t4,end_loop_seg 
+		jal fibonachi #function that calculate the fibonachi of a2
+		move $a0,$v0	#the return of fibonachi function
+		li $v0,1  #print the return
+		syscall
+		li $v0,4
+		la $a0,n #print \n
+		syscall
+	
+		addi $a1,$a1,1 # a = a + 1
+		
+		j loop_seg
+		
+	end_loop_seg :
+		lw $a1, 0($sp)		
+		lw $a2, 4($sp)
+		lw $ra, 8($sp)
+		addi $sp,$sp,12 
+		
+		jr $ra
+	
+	
+	
+			
+fibonachi :
+	addi $sp,$sp,-8 
+	sw $a1, 0($sp)		
+	sw $ra, 4($sp)
+	
+	addi $t0,$zero,1 #fib(0)
+	addi $t1,$zero,1 #fib(1)
+	addi $t3,$zero,1 #stop condition
+	
+	loop_fib :
+		beq $a1,$t3,end_loop_fib 
+		add $v0,$t0,$t1 #the next fibonachi number
+		move $t0,$t1 #fib(n) = fib(n+1)
+		move $t1,$v0 #fib(n+1) = fib(n+2)
+		addi $a1,$a1,-1 
+		
+		j loop_fib
+		
+	end_loop_fib :
+		lw $a1, 0($sp)		
+		lw $ra, 4($sp)
+		addi $sp,$sp,8 
+		
+		jr $ra
 
 end:
 	li $v0, 10	# c�digo para finalizar o programa
